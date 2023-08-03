3f992dde7d83e970226a2dc03bd8a0030da5be05
septiadi70 <septiadi70@gmail.com>
2023-08-03 12:11:56 +0700

    initial commit

---
+++ TrinityTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrinityTest
+//
+//  Created by Andi Septiadi on 03/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
